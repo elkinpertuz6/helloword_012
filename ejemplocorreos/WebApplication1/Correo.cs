0abad88cd87e9ae444027619e84b8000a83417e7
elkinpertuz6 <elccoco1995@hotmail.com>
2016-02-11 10:50:13 -0500

    codigocorreo

---
+++ ejemplocorreos/WebApplication1/Correo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace WebApplication1
+{
+    public class Correo
+    {
+        Boolean estado = true;
+        string merror;
+        public Correo(string destinatario, string Asunto, string mensaje)
+        {
+            MailMessage Correo = new MailMessage();
+            SmtpClient protocolo = new SmtpClient();
+
+            Correo.To.Add(destinatario);
+            Correo.From = new MailAddress("elccoco1995@hotmail.com", "rosa elena", System.Text.Encoding.UTF8);
+            Correo.Subject = Asunto;
+            Correo.SubjectEncoding = System.Text.Encoding.UTF8;
+            Correo.Body = mensaje;
+            Correo.BodyEncoding = System.Text.Encoding.UTF8;
+            Correo.IsBodyHtml = false;
+            protocolo.Credentials = new System.Net.NetworkCredential("elccoco1995@hotmail.com", "santander");
+            protocolo.Port = 587;
+            protocolo.Host = "smtp.gmail.com";
+            protocolo.EnableSsl = true;
+            try
+            {
+                protocolo.Send(Correo);
+            }
+            catch(SmtpException error)
+            {
+                estado = false;
+                merror = error.Message.ToString();
+            }
+
+          
+
+        }
+
+        
+        public Boolean Estado
+        {
+            get { return estado; }
+        }
+
+        public string mensade_error
+        {
+            get { return merror; }
+        }
+    }
+
+}
---
+++ ejemplocorreos/WebApplication1/enviacorreo.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class enviacorreo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Correo objCorreo = new Correo("eepertuz6@misena.edu.co","prueba de notificaciones","eva sabeeee ");
+            if (objCorreo.Estado)
+            {
+                Response.Write("la prueba ha sido un exito");
+            }
+            else
+            {
+                Response.Write("prueba fallida"+objCorreo.mensade_error);
+            }
+            
+    }
+    }
+}
